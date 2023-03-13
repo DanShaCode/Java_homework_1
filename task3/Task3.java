ceb4a8ee471abf1e2e1d444251091637aff73539
Danis Sharipov <danshadance@mail.ru>
2023-03-13 11:23:27 +0600

    calc change

--- task3/Task3.java
+++ task3/Task3.java
@@ -1,117 +1,148 @@
 package task3;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Task3 {
 
-    private static void calc(Scanner scanner) {
-
+    private static void operationsMenu(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
         System.out.println();
-        System.out.println("      Калькулятор");
+        System.out.println("      МЕНЮ ОПЕРАЦИЙ");
+        System.out.println("===========================");
         System.out.println();
-        System.out.println("     ОСНОВНОЕ МЕНЮ");
+        System.out.println("1. Сложение");
         System.out.println();
-        System.out.println("=========================");
+        System.out.println("2. Вычитание");
         System.out.println();
-        System.out.println("1. Сложение чисел");
+        System.out.println("3. Умножение");
         System.out.println();
-        System.out.println("2. Вычитание чисел");
-        System.out.println();
-        System.out.println("3. Умножение чисел");
-        System.out.println();
-        System.out.println("4. Деление чисел");
+        System.out.println("4. Деление");
         System.out.println();
         System.out.print("Введите цифру из меню: ");
         int userInput = scanner.nextInt();
         if (userInput == 1) {
-            sumNum(scanner);
+            sumNum(numFormat, scanner, res);
         }
         if (userInput == 2) {
-            diffNum(scanner);
+            diffNum(numFormat, scanner, res);
         }
         if (userInput == 3) {
-            prodNum(scanner);
+            prodNum(numFormat, scanner, res);
         }
         if (userInput == 4) {
-            divideNum(scanner);
+            divideNum(numFormat, scanner, res);
         }
-
     }
 
-    private static void checker(double op) {
-        int flag = 0;
-        for (int i = 1; i <= 10 ; i++){
-            if (op % i == 0){
-                flag++;
-                break;
+    private static void table(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
+        System.out.println();
+        System.out.println("ТЕКУЩЕЕ ЗНАЧЕНИЕ НА ТАБЛО");
+        System.out.println("=========================");
+        System.out.println("            " + numFormat.format(res));
+        System.out.println("=========================");
+        System.out.println();
+        System.out.print("Введите число: ");
+        double userIput = scanner.nextDouble();
+        res = userIput;
+        calc(numFormat, scanner, res);
+    }
+
+    private static void calc(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
+        System.out.println();
+        System.out.println(" ТЕКУЩЕЕ ЗНАЧЕНИЕ НА ТАБЛО");
+        System.out.println("===========================");
+        System.out.println("             " + numFormat.format(res));
+        System.out.println("===========================");
+        System.out.println();
+        System.out.println();
+        System.out.println("ОСНОВНОЕ МЕНЮ КАЛЬКУЛЯТОРА");
+        System.out.println("===========================");
+        System.out.println();
+        System.out.println("1. Ввести число на табло");
+        System.out.println();
+        System.out.println("2. Операции");
+        System.out.println();
+        System.out.println("3. Очистить табло");
+        System.out.println();
+        System.out.println("4. Закрыть калькулятор");
+        System.out.println();
+        System.out.print("Введите цифру из меню: ");
+        int userInput = scanner.nextInt();
+        if (userInput == 1) {
+            table(numFormat, scanner, res);
+        }
+        if (userInput == 2) {
+            operationsMenu(numFormat, scanner, res);
+        }
+        if (userInput == 3) {
+            res = 0;
+            calc(numFormat, scanner, res);
+        }
+        if (userInput == 4) {
+            System.out.println("\033[H\033[2J");
+            return;
+        }
+    }
+
+    private static void sumNum(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
+        System.out.println();
+        System.out.print("Введите число: ");
+        double num = scanner.nextDouble();
+        double op = res + num;
+        calc(numFormat, scanner, op);
+    }
+
+    private static void diffNum(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
+        System.out.println();
+        System.out.print("Введите число: ");
+        double num = scanner.nextDouble();
+        double op = res - num;
+        calc(numFormat, scanner, op);
+    }
+
+    private static void prodNum(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
+        System.out.println();
+        System.out.print("Введите число: ");
+        double num = scanner.nextDouble();
+        double op = res * num;
+        calc(numFormat, scanner, op);
+    }
+
+    private static void divideNum(DecimalFormat numFormat, Scanner scanner, double res) {
+        System.out.println("\033[H\033[2J");
+        System.out.println();
+        System.out.print("Введите число: ");
+        double num = scanner.nextDouble();
+        System.out.println();
+        if (num == 0) {
+            System.out.println("ОШИБКА");
+            System.out.println();
+            System.out.print("Введите q для возврата в Основное меню: ");
+            String userWait = scanner.next();
+            if (userWait == "q" || userWait == "Q"){
+                calc(numFormat, scanner, res);
+            }  else {
+                calc(numFormat, scanner, res);
             }
-        }
-        if (flag > 0){
-            int result = (int) op;
-            System.out.println();
-            System.out.println("Ответ: " + result);
-        } else {
-            System.out.println();
-            System.out.print("Ответ: " + op);
-            System.out.println();
-        }
-    }
-
-    private static void sumNum(Scanner scanner) {
-        System.out.println();
-        System.out.print("Введите первое число: ");
-        var num1 = scanner.next();
-        System.out.println();
-        System.out.print("Введите второе число: ");
-        var num2 = scanner.next();
-        double a = Double.parseDouble(num1);
-        double b = Double.parseDouble(num2);
-        double op = a + b;
-        checker(op);
-    }
-
-    private static void diffNum(Scanner scanner) {
-        System.out.println();
-        System.out.print("Введите первое число: ");
-        var num1 = scanner.next();
-        System.out.println();
-        System.out.print("Введите второе число: ");
-        var num2 = scanner.next();
-        double a = Double.parseDouble(num1);
-        double b = Double.parseDouble(num2);
-        double op = a - b;
-        checker(op);
-    }
-
-    private static void prodNum(Scanner scanner) {
-        System.out.println();
-        System.out.print("Введите первое число: ");
-        var num1 = scanner.next();
-        System.out.println();
-        System.out.print("Введите второе число: ");
-        var num2 = scanner.next();
-        double a = Double.parseDouble(num1);
-        double b = Double.parseDouble(num2);
-        double op = a * b;
-        checker(op);
-    }
-
-    private static void divideNum(Scanner scanner) {
-        System.out.println();
-        System.out.print("Введите первое число: ");
-        var num1 = scanner.next();
-        System.out.println();
-        System.out.print("Введите второе число: ");
-        var num2 = scanner.next();
-        double a = Double.parseDouble(num1);
-        double b = Double.parseDouble(num2);
-        double op = a / b;
-        checker(op);
+        }  else {
+            double op = res / num;
+            calc(numFormat, scanner, op);
+            }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        calc(scanner);
+        DecimalFormat numFormat = new DecimalFormat();
+        numFormat.setDecimalSeparatorAlwaysShown(false);
+        double res = 0;
+        numFormat.format(res);
+        calc(numFormat, scanner, res);
         scanner.close();
     }
 }
